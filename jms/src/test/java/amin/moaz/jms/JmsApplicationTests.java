4fe98c226d99002ccf6cb0af9ec293c346c8df18
Moaz Amin <moazamin06@gmail.com>
2023-07-08 23:03:02 +0300

    Initial commit

---
+++ jms/src/test/java/amin/moaz/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package amin.moaz.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
